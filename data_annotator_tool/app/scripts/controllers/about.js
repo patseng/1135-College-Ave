a617f13ad591c78a9fdead36b6c020be84732868
Peter Tseng <patseng@stanford.edu>
2014-09-04 16:06:00 -0700

    Initial Commit For Data Annotator Tool.

---
+++ data_annotator_tool/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dataAnnotatorToolApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dataAnnotatorToolApp
+ */
+angular.module('dataAnnotatorToolApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ data_annotator_tool/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dataAnnotatorToolApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dataAnnotatorToolApp
+ */
+angular.module('dataAnnotatorToolApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
